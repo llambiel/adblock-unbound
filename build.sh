dad256d1041865b21ad0cd8f9c3b8bf1b66909fb
Loic Lambiel <loic@lambiel.net>
2020-04-27 19:47:54 +0000

    some fixes

--- build.sh
+++ build.sh
@@ -2,5 +2,6 @@
 rm hosts*
 wget https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling/hosts
 cat hosts | grep '^0\.0\.0\.0' | awk '{print "local-zone: \""$2"\" redirect\nlocal-data: \""$2" A 0.0.0.0\""}' > ads.conf
+echo 'server: ' | cat - ads.conf > temp && mv temp ads.conf
 sudo cp ads.conf /etc/unbound/unbound.conf.d/ads.conf
-sudo service unbound reload
+#sudo systemctl reload unbound
